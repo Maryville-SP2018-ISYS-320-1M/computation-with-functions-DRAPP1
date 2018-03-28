b08dfa9daa92ae573509253b188695e78c92a0cd
Dave <37315071+DRAPP1@users.noreply.github.com>
2018-03-28 16:46:11 -0500

    s

--- src/P1_MysteryReturn.java
+++ src/P1_MysteryReturn.java
@@ -1,21 +1,39 @@
 /*
-	ISYS 320
-	Name(s):
-	Date: 
+    ISYS 320
+	Name(s): Dave Rapp
+	Date: 3-26-18
 */
 
 /*
  1. Your output prediction (do yourself a favor and go step by step through the code, recording the value of each variable at each method call):
-  
+  I honestly have no idea what this program is going to do. My wild guess is that it will print the results of the computations based on the code
   
  */
 
 /*
  3. Were you correct? Explain if you were not (what did you learn?)
+ Yes I was although I was very generic in my prediction. Still having trouble with this though.
  
  
  */
 public class P1_MysteryReturn {
+	public static void main(String[] args) {
+        int x = 1, y = 2, z = 3;
+        z = mystery( x, z, y);
+        System.out.println( x + " " + y + " " + z);
+        x = mystery( z, z, x);
+        System.out.println( x + " " + y + " " + z);
+        y = mystery( y, y, z);
+        System.out.println( x + " " + y + " " + z);
 
+    }
+    
+    public static int mystery( int z, int x, int y) {
+        z--;
+        x = 2 * y + z;
+        y = x - 2;
+        System.out.println(y + " " + z );
+        return x;
+    }
 
 }
--- src/P4_FixTempConverter.java
+++ src/P4_FixTempConverter.java
@@ -1,25 +1,31 @@
 /*
-	ISYS 320
-	Name(s):
-	Date: 
+ISYS 320
+	Name(s):Dave Rapp
+	Date: 3-26-18
 */
 
 /*
  What were the errors you found?
+ You have to move the print line down otherwise it will not print the computed values
  
  
  */
 
+import java.util.Scanner;
+
 public class P4_FixTempConverter {
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner ( System.in );
 		double tempf = 98.6;
 		double tempc = 0.0;
 		ftoc(tempf, tempc);
-		System.out.println("Body temp in C is: " + tempc);
+		
 	}
 
 	// converts Fahrenheit temperatures to Celsius
 	public static void ftoc(double tempf, double tempc) {
 	      tempc = (tempf - 32) * 5 / 9;
+	      System.out.println("Body temp in C is: " + tempc);
     }
 }
